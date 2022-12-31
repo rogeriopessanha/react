57663157e87c77ab49047e4faaae74aa3d209135
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2022-12-31 16:26:30 -0300

    att

--- meu-app03/src/App.js
+++ meu-app03/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      nome: "Rogerio",
+      contador: 0
+    }
+  }
+
+  aumentar(){
+    console.log('Aumentou')
+  }
+
+  render() {
+    return (
+      <div>
+
+        <h1>Contador</h1>
+
+        <h3>
+          <button>-</button> 
+          {this.state.contador} 
+          <button onclick={this.aumentar}>+</button>
+        </h3>
+
+      </div>
+    );
+  }
 }
 
 export default App;
