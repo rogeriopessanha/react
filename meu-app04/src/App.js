65349d2d798fa77154f2173781df2c511619b492
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-01 23:43:02 -0300

    att

--- meu-app04/src/App.js
+++ meu-app04/src/App.js
@@ -3,10 +3,30 @@ import React, {Component} from 'react';
 
 class App extends Component{
 
+  constructor(props){
+    super(props)
+    this.state = {
+      hora: '00:00:00'
+    }
+  }
+
+  componentDidMount(){
+
+    setInterval(() => {
+      this.setState({hora: new Date().toLocaleTimeString() })
+    }, 1000)
+  }
+
+  componentDidUpdate(){
+    console.log('atualizou')
+  }
+
+  // shouldComponentUpdate(){}
+
   render(){
     return(
       <div>
-        <h1>Meu Projeto</h1>
+        <h1>Meu Projeto {this.state.hora}</h1>
       </div>
     )
   }
