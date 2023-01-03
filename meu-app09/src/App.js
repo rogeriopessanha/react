402ae7ff9bc174b7cfd7b678fcc1d1f75c0d4b43
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-03 14:39:06 -0300

    att

--- meu-app09/src/App.js
+++ meu-app09/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      nome: '',
+      email: '',
+      senha: ''
+    }
+
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Novo usuario</h1>
+
+        <form>
+          <input type="text" value={this.state.nome} onChange={(e) => this.setState({nome: e.target.value})} />
+          <br/>
+
+          <input type="email" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})} />
+          <br/>
+
+          <input type="password" value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value})} />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
