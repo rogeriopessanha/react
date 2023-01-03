4f2370ffd9d6197b089e2642efd4946745bc6c1a
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-03 15:22:08 -0300

    att

--- meu-app09/src/App.js
+++ meu-app09/src/App.js
@@ -7,9 +7,25 @@ class App extends Component{
     this.state = {
       nome: '',
       email: '',
-      senha: ''
+      senha: '',
+      error: ''
     }
 
+    this.cadastrar = this.cadastrar.bind(this)
+
+  }
+
+  cadastrar(e){
+    const {nome, email, senha} = this.state
+
+    if (nome !== '' && email !== '' && senha !== '') {
+      
+      alert(`Nome: ${nome} \n Email: ${email} \n Senha: ${senha}`)
+    }else{
+      this.setState({error: 'Ops, Parece que está faltando alguma informação'})
+    }
+
+    e.preventDefault()
   }
 
   render(){
@@ -17,14 +33,23 @@ class App extends Component{
       <div>
         <h1>Novo usuario</h1>
 
-        <form>
+        {this.state.error && <p>{this.state.error}</p>}
+
+
+        <form onSubmit={this.cadastrar}>
+          <label>Nome: </label>
           <input type="text" value={this.state.nome} onChange={(e) => this.setState({nome: e.target.value})} />
           <br/>
 
+          <label>Email: </label>
           <input type="email" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})} />
           <br/>
 
+          <label>Senha: </label>
           <input type="password" value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value})} />
+          <br/>
+
+          <button type='submit'>Cadastrar</button>
         </form>
       </div>
     )
