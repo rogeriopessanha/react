199f04fdd16e41d5474a9e72a65aa19ec2426021
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-03 20:27:58 -0300

    att

--- meu-app08/src/App.js
+++ meu-app08/src/App.js
@@ -5,51 +5,54 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      email: '',
-      senha: '',
-      sexo: 'masculino'
+      form:{
+        nome: '',
+        email: '',
+        senha: '',
+        sexo: ''
+      }
     }
 
-    this.trocaEmail = this.trocaEmail.bind(this)
-    this.trocaSexo = this.trocaSexo.bind(this)
-
+    this.dadosForm = this.dadosForm.bind(this)
   }
 
-  trocaEmail(e){
-    let valorDigitado = e.target.value
-    this.setState({email: valorDigitado})
+  dadosForm(e){
+    let form = this.state.form
+    form[e.target.name] = e.target.value
+    this.setState({form: form})
   }
 
-  trocaSexo(e){
-    let valorDigitado = e.target.value
-    this.setState({sexo: valorDigitado})
-  }
+  
 
   render(){
     return(
       <div>
         <h2>Login</h2>
-        Email:
-        <input type="email" name='email' value={this.state.email} onChange={this.trocaEmail}/>
 
+        Nome:
+        <input type="text" name='nome' value={this.state.form.nome} onChange={this.dadosForm}/>
+        <br/>
+
+        Email:
+        <input type="email" name='email' value={this.state.form.email} onChange={this.dadosForm}/>
         <br/>
 
         Senha:
-        <input type="password" name='senha' value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value})}/>
-      
+        <input type="password" name='senha' value={this.state.form.senha} onChange={this.dadosForm}/>
         <br/>
 
         Sexo:
-        <select name='sexo' value={this.state.sexo} onChange={this.trocaSexo}>
+        <select name='sexo' value={this.state.form.sexo} onChange={this.dadosForm}>
           <option value="masculino">Masculino</option>
           <option value="feminino">Feminino</option>
         </select>
 
 
         <div>
-          <h3>{this.state.email}</h3>
-          <h3>{this.state.senha}</h3>
-          <h3>{this.state.sexo}</h3>
+          <h3>{this.state.form.nome}</h3>
+          <h3>{this.state.form.email}</h3>
+          <h3>{this.state.form.senha}</h3>
+          <h3>{this.state.form.sexo}</h3>
         </div>
       </div>
     )
