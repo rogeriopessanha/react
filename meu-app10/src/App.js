d07c80e4e1c297c6f5af0af13c0b4e079fd4aa1d
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-04 14:12:58 -0300

    att

--- meu-app10/src/App.js
+++ meu-app10/src/App.js
@@ -1,10 +1,33 @@
 
-import React from 'react'
+import React, {useState} from 'react'
 
 function App() {
+
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar React Hooks'
+  ])
+
+  const [input, setInput] = useState('') 
+
+  function handleAdd(){
+    setTarefas([...tarefas, input])
+    setInput('')
+  }
+
+
+
   return (
     <div>
-      <h1>Hooks</h1>
+      <ul>
+        {tarefas.map(tarefa =>(
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+
+      <button type='button' onClick={handleAdd}>Adicionar</button>
+
     </div>
   );
 }
