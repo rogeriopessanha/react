06f2ca7ec3fe0ce811b7a0468df00f8af757f1e6
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-04 01:03:23 -0300

    att

--- projeto-cronometro/src/App.js
+++ projeto-cronometro/src/App.js
@@ -6,20 +6,56 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-
+      numero: 0,
+      botao: 'COMEÇAR'
     }
 
+    this.timer = null
+    this.vai = this.vai.bind(this)
+    this.limpar = this.limpar.bind(this)
+
+  }
+
+  vai(){
+
+    let state = this.state
+
+    if (this.timer !== null) {
+      clearInterval(this.timer)
+      this.timer = null
+      state.botao = 'COMEÇAR'
+    }else{
+      this.timer = setInterval(() =>{
+        let state = this.state
+        state.numero += 0.1
+        this.setState(state)
+      }, 100)
+      state.botao = 'PAUSAR'
+    }
+    this.setState(state)
+  }
+
+  limpar(){
+    if (this.timer !== null) {
+      clearInterval(this.timer)
+      this.timer = null
+    }
+
+    let state = this.state
+    state.numero = 0
+    state.botao = 'COMEÇAR'
+    this.setState(state)
   }
 
   render(){
     return(
       <div className="container">
         <img src={require('./assets/cronometro.png')} alt="" />
-        <a className="timer">0.0</a>
+        <a className="timer">{this.state.numero.toFixed(1)}</a>
 
         <div className="areaBtn">
-          <a className="botao">VAI</a>
-          <a className="botao">LIMPAR</a>
+          <a className="botao" onClick={this.vai}>{this.state.botao}</a>
+          <a className="botao" onClick={this.limpar}>LIMPAR</a>
         </div>
       </div>
     )
