cfc1d66804fc8afc6dced115298edef182eaf163
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-05 13:14:19 -0300

    att

--- projeto-filme/src/pages/Home/index.js
+++ projeto-filme/src/pages/Home/index.js
@@ -1,12 +1,15 @@
 
 import { useEffect, useState } from "react"
 import api from '../../services/api'
+import { Link } from 'react-router-dom'
+import './home.css'
 
 // URL da api: movie/now_playing?api_key=88e105eb0a18788085edf385811cdb97
 
 function Home() {
 
     const [filmes, setFilmes] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
 
@@ -18,16 +21,37 @@ function Home() {
                 }
             })
 
-            console.log(response.data.results)
+            // console.log(response.data.results.slice(0, 10))
+            setFilmes(response.data.results.slice(0, 10))
+            setLoading(false)
         }
 
         loadFilmes()
 
     }, [])
 
+
+    if (loading) {
+        return(
+            <div className="loading">
+                <h2>Carregando filmes...</h2>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>BEM VINDO(A) A HOME</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) => {
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
--- projeto-filme/src/routes.js
+++ projeto-filme/src/routes.js
@@ -4,6 +4,8 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Filme from './pages/Filme'
 
+import Erro from './pages/Erro'
+
 import Header from './components/Header'
 
 function RoutesApp() {
@@ -13,6 +15,8 @@ function RoutesApp() {
         <Routes>
             <Route path='/' element={ <Home/> } />
             <Route path='/filme/:id' element={ <Filme/> } />
+
+            <Route path='*' element={ <Erro/>}></Route>
         </Routes>
         </BrowserRouter>
     )
