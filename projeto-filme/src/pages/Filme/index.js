a7fe7389d4302608dfc41f3f53b42442ab0f4468
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-05 02:10:37 -0300

    att

---
+++ projeto-filme/src/pages/Filme/index.js
@@ -0,0 +1,9 @@
+function Filme() {
+    return(
+        <div>
+            <h1>BEM VINDO(A) A PÁGINA DE FILMES</h1>
+        </div>
+    )
+}
+
+export default Filme
---
+++ projeto-filme/src/pages/Home/index.js
@@ -0,0 +1,9 @@
+function Home() {
+    return(
+        <div>
+            <h1>BEM VINDO(A) A HOME</h1>
+        </div>
+    )
+}
+
+export default Home
