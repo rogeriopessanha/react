bf736f3875b68563b7c0afa21dbbc4288c7845d6
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-06 03:08:15 -0300

    att

--- meu-app13/src/App.js
+++ meu-app13/src/App.js
@@ -1,10 +1,44 @@
 
+import { useState } from 'react';
 import { db} from './firebaseConnection'
+import { doc, setDoc} from 'firebase/firestore'
+import './app.css'
+// import { async } from '@firebase/util';
 
 function App() {
+
+  const [titulo, setTitulo] = useState('')
+  const [autor, setAutor] = useState('')
+
+  async function handleAdd() {
+    await setDoc(doc(db, "posts", 123456), {
+      titulo: titulo,
+      autor: autor,
+    })
+    .then(() =>{
+      console.log("dados registrados no banco")
+    })
+
+    .catch((error) =>{
+      console.log("deu erro" + error)
+    })
+  }
+
   return (
     <div>
-      <h1>testando</h1>
+      <h1>testando ;) </h1>
+
+      <div className="container">
+        <label>Titulo:</label> <br/>
+
+        <textarea type="text" placeholder='Digite seu titulo' value={titulo} onChange={(e) => setTitulo(e.target.value)}/> <br/>
+
+        <label>Autor:</label> <br/>
+
+        <input type="text" placeholder='Autor do post' value={autor} onChange={(e) => setAutor(e.target.value)} /> <br/>
+
+        <button onClick={handleAdd}>Cadastrar</button>
+      </div>
     </div>
   );
 }
