0867c42bdf2ec21e91949c6c230e2eceec09e553
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-20 23:55:32 -0300

    att

--- projeto-reserva/src/App.js
+++ projeto-reserva/src/App.js
@@ -3,15 +3,20 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import Header from './components/Header';
+import { Provider } from 'react-redux';
+import store from './store';
+
 
 
 export default function App() {
   return (
 
-      <BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
       <Header/>
         <Routes/>
-      </BrowserRouter>  
+      </BrowserRouter>
+      </Provider>  
   );
 }
 
---
+++ projeto-reserva/src/store/index.js
@@ -0,0 +1,10 @@
+
+import {createStore} from 'redux'
+
+function reserva() {
+    return[]
+}
+
+const store = createStore(reserva)
+
+export default store
