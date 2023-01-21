fc2a909d6f51719e2d1b7a64f71b80d365ff3bcb
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-21 17:10:15 -0300

    att

--- projeto-reserva/src/pages/Reservas/index.js
+++ projeto-reserva/src/pages/Reservas/index.js
@@ -1,10 +1,19 @@
+
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { MdDelete } from 'react-icons/md';
 import './style.css';
 
 export default function Reservas() {
+ const dispatch = useDispatch();
  const reserves = useSelector(state => state.reserve);
+
+ function handleRemove(id){
+  dispatch({
+    type: 'REMOVE_RESERVE',
+    id,
+  })
+ }
 
  return (
    <div>
@@ -20,7 +29,7 @@ export default function Reservas() {
         <span>Quantidade: {reserve.amount}</span>
         <button
         type="button"
-        onClick={()=> {}}
+        onClick={()=> handleRemove(reserve.id) }
         >
         <MdDelete size={20} color="#191919" />
       </button>
--- projeto-reserva/src/store/modules/reserve/reducer.js
+++ projeto-reserva/src/store/modules/reserve/reducer.js
@@ -1,12 +1,34 @@
 
-export default function reserve(state = [], action) {
-    switch (action.type) {
-        case 'ADD_RESERVE':
-            return [...state,{
-             ...action.trip,
-             amount: 1,  
-            } ];
-        default:
-            return state
-    }
+import produce from 'immer';
+
+export default function reserve(state = [], action ){
+  
+  switch(action.type){
+    case 'ADD_RESERVE':
+      return produce(state, draft => {
+        const tripIndex = draft.findIndex(trip => trip.id === action.trip.id);
+
+        if(tripIndex >= 0){
+          draft[tripIndex].amount +=1;
+          
+        }else{
+          draft.push({
+            ...action.trip,
+            amount:1,
+          });
+        }
+
+      });
+    case 'REMOVE_RESERVE':
+      return produce(state, draft => {
+        const tripIndex = draft.findIndex(trip => trip.id === action.id);
+
+        if(tripIndex >= 0){
+          draft.splice(tripIndex, 1);
+        }
+
+      });
+    default:
+      return state;
+  }
 }
