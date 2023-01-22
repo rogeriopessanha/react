b288b40ee179e23c6c8867ebcf67474fce94bce0
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-21 23:28:22 -0300

    att

--- projeto-reserva/src/pages/Reservas/index.js
+++ projeto-reserva/src/pages/Reservas/index.js
@@ -1,45 +1,54 @@
 
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { MdDelete } from 'react-icons/md';
+import { removeReserve } from '../../store/modules/reserve/actions'
+import { MdDelete, MdAddCircle, MdRemoveCircle } from 'react-icons/md';
 import './style.css';
 
 export default function Reservas() {
- const dispatch = useDispatch();
- const reserves = useSelector(state => state.reserve);
+  const dispatch = useDispatch();
+  const reserves = useSelector(state => state.reserve);
 
- function handleRemove(id){
-  dispatch({
-    type: 'REMOVE_RESERVE',
-    id,
-  })
- }
+  function handleRemove(id) {
+    dispatch(removeReserve(id))
+  }
 
- return (
-   <div>
-    <h1 className="title">Voce solicitou {reserves.length} reservas</h1>
+  return (
+    <div>
+      <h1 className="title">Voce solicitou {reserves.length} reservas</h1>
 
-    {reserves.map(reserve => (
-      <div className="reservas" key={reserve.id}>
-        <img
-        src={reserve.image}
-        alt={reserve.title}
-        />
-        <strong>{reserve.title}</strong>
-        <span>Quantidade: {reserve.amount}</span>
-        <button
-        type="button"
-        onClick={()=> handleRemove(reserve.id) }
-        >
-        <MdDelete size={20} color="#191919" />
-      </button>
+      {reserves.map(reserve => (
+        <div className="reservas" key={reserve.id}>
+          <img
+            src={reserve.image}
+            alt={reserve.title}
+          />
+          <strong>{reserve.title}</strong>
+
+          <div id='amount'>
+            <button type='button' onClick={() =>{}}>
+            <MdRemoveCircle size={25} color="#191919" />
+            </button>
+
+            <input type='text' readOnly value={reserve.amount} />
+
+            <button type='button' onClick={() =>{}}>
+            <MdAddCircle size={25} color="#191919" />
+            </button>
+          </div>
+          <button
+            type="button"
+            onClick={() => handleRemove(reserve.id)}
+          >
+            <MdDelete size={20} color="#191919" />
+          </button>
+        </div>
+      ))}
+
+      <footer>
+        <button type="button">Solicitar Reservas</button>
+      </footer>
+
     </div>
-    ))}
-
-    <footer>
-      <button type="button">Solicitar Reservas</button>
-    </footer>
-
-   </div>
- );
+  );
 }
