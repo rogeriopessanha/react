f59b4c799648163d2fe0c343bbffd4cac1fcb809
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-23 00:13:55 -0300

    att

--- projeto-reserva/src/index.js
+++ projeto-reserva/src/index.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -6,4 +5,3 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
- 
--- projeto-reserva/src/store/modules/reserve/actions.js
+++ projeto-reserva/src/store/modules/reserve/actions.js
@@ -1,35 +1,3 @@
-// export function addReserveRequest(id){
-//   return {
-//     type: 'ADD_RESERVE_REQUEST',
-//     id
-//   }
-// }
-
-// export function addReserveSuccess(trip){
-//   return {
-//     type: 'ADD_RESERVE_SUCCESS',
-//     trip
-//   }
-// }
-
-// export function removeReserve(id){
-//   return {
-//     type: 'REMOVE_RESERVE',
-//     id,
-//   }
-// }
-
-// export function updateAmountReserve(id, amount){
-//   return {
-//     type: 'UPDATE_RESERVE',
-//     id,
-//     amount
-//   }
-// }
-
-
-
-
 export function addReserveRequest(id){
   return {
     type: 'ADD_RESERVE_REQUEST',
@@ -51,9 +19,17 @@ export function removeReserve(id){
   }
 }
 
-export function updateAmountReserve(id, amount){
+export function updateAmountRequest(id, amount){
   return {
-    type: 'UPDATE_RESERVE',
+    type: 'UPDATE_RESERVE_REQUEST',
+    id,
+    amount
+  }
+}
+
+export function updateAmountSuccess(id, amount){
+  return {
+    type: 'UPDATE_RESERVE_SUCCESS',
     id,
     amount
   }
